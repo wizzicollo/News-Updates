ffd14657f836ba83228ca6195a298e6e3e7e0ccb
Frozen Feelings <kiprutohcollo@gmail.com>
2020-04-27 00:13:26 +0300

    created view.py to import view functions

---
+++ views.py
@@ -0,0 +1,22 @@
+from flask import render_template
+from app import app
+from .requests import get_articles,get_sources
+
+# Views
+@app.route('/')
+def index():
+    '''
+    View root page function that returns the index page and its data
+    '''
+    news_source = get_sources()
+    
+    title = 'Home - Welcome to The best News  Highlight Website Online'
+    return render_template('index.html', title = title,news_source = news_source )
+
+
+
+@app.route('/articles/<id>')
+def news(id):
+    article = get_articles(id)
+    print(article)
+    return render_template('articles.html',article = article)

19c81ed215516206f1d2bd6aeb6986f2d988cff5
Frozen Feelings <kiprutohcollo@gmail.com>
2020-04-27 01:06:18 +0300

    stored news api base url in config.py

--- app/config.py
+++ app/config.py
@@ -2,7 +2,9 @@ class Config:
     '''
     General configuration parent class
     '''
-
+    NEWS_API_BASE_URL = 'https://newsapi.org/v2/sources?language=en&apiKey='
+    ARTICLE_API_BASE_URL = 'https://newsapi.org/v2/everything?sources={}&apiKey={}'
+    NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
 
 
 class ProdConfig(Config):
